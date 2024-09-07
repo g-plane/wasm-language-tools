b98118b468df2623dd0be030852d3e220e27234d
Pig Fang <g-plane@hotmail.com>
2024-09-07 17:18:53 +0800

    refactor(parser): better instant proof of concept

--- crates/parser/examples/debug_cst.rs
+++ crates/parser/examples/debug_cst.rs
@@ -4,7 +4,7 @@ fn main() {
     let input = fs::read_to_string(env::args().nth(1).unwrap()).unwrap();
     let mut parser = wat_parser::Parser::new(&input);
     let tree = parser.parse();
-    similar_asserts::assert_eq!(input, tree.to_string());
     println!("{tree:#?}");
     dbg!(parser.errors());
+    similar_asserts::assert_eq!(input, tree.to_string());
 }
