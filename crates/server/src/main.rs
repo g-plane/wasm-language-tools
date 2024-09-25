7665294e78bfb8e6f1c4c1ba08ff85d9af03f607
Pig Fang <g-plane@hotmail.com>
2024-09-25 10:39:40 +0800

    feat(server): add logging

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -1,6 +1,16 @@
+use tracing::{event, Level};
+use tracing_subscriber::prelude::*;
 use wat_server::Server;
 
 fn main() -> anyhow::Result<()> {
+    if let Ok(layer) = tracing_journald::layer() {
+        tracing_subscriber::registry().with(layer).init();
+    }
+
+    let span = tracing::span!(Level::TRACE, "wat_server");
+    let _enter = span.enter();
+
+    event!(Level::INFO, "wat_server starting");
     let mut server = Server::default();
     server.run()?;
     Ok(())
