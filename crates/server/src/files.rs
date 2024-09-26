8d7a9fb670c01e4ec8be68de6c0438c9cb443e31
Pig Fang <g-plane@hotmail.com>
2024-09-26 22:28:21 +0800

    fix(server): fix compilation

--- crates/server/src/files.rs
+++ crates/server/src/files.rs
@@ -1,8 +1,6 @@
 use crate::diag::Diagnostic;
-use ahash::AHashMap;
-use comemo::Tracked;
 use line_index::{LineIndex, TextSize};
-use lsp_types::{Position, Range, Uri};
+use lsp_types::{Position, Range};
 use rowan::ast::AstNode;
 use wat_parser::Parser;
 use wat_syntax::ast::Root;
@@ -41,29 +39,4 @@ impl File {
             syntax_errors,
         }
     }
-}
-
-#[derive(Clone, Debug, Default)]
-pub struct Files(AHashMap<Uri, String>);
-
-#[comemo::track]
-impl Files {
-    pub fn read(&self, uri: &Uri) -> String {
-        self.0.get(uri).cloned().unwrap_or_default()
-    }
-}
-
-impl Files {
-    pub fn write(&mut self, uri: Uri, source: String) {
-        self.0.insert(uri, source);
-    }
-
-    pub fn remove(&mut self, uri: &Uri) {
-        self.0.remove(uri);
-    }
-}
-
-#[comemo::memoize]
-pub(crate) fn get_line_index(uri: &Uri, files: Tracked<Files>) -> LineIndex {
-    LineIndex::new(&files.read(uri))
 }
