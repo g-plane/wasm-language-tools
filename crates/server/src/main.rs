84443f4ea876b10c383b4d574e2830d9fa5703aa
Pig Fang <g-plane@hotmail.com>
2024-09-27 22:32:04 +0800

    feat(server): detect simple CLI arguments

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -1,11 +1,19 @@
 mod server;
 
+use std::env;
 use tracing::{event, Level};
 use tracing_subscriber::prelude::*;
 
 fn main() -> anyhow::Result<()> {
-    if let Ok(layer) = tracing_journald::layer() {
-        tracing_subscriber::registry().with(layer).init();
+    if env::args().any(|arg| arg == "-v" || arg == "-V" || arg == "--version") {
+        println!("wat_server v{}", env!("CARGO_PKG_VERSION"));
+        return Ok(());
+    }
+
+    if env::args().any(|arg| arg == "--debug") {
+        if let Ok(layer) = tracing_journald::layer() {
+            tracing_subscriber::registry().with(layer).init();
+        }
     }
 
     let span = tracing::span!(Level::TRACE, "wat_server");
