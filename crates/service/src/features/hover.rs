de08ca66b4b9ad0f5795128383a07623fe39c0ee
Pig Fang <g-plane@hotmail.com>
2024-10-09 17:53:43 +0800

    feat(service): support hover on definition

--- crates/service/src/features/hover.rs
+++ crates/service/src/features/hover.rs
@@ -1,6 +1,6 @@
 use super::find_meaningful_token;
 use crate::{
-    binder::{SymbolItemKind, SymbolTablesCtx},
+    binder::{SymbolItem, SymbolItemKind, SymbolTablesCtx},
     files::FilesCtx,
     helpers,
     types_analyzer::TypesAnalyzerCtx,
@@ -8,7 +8,7 @@ use crate::{
 };
 use lsp_types::{Hover, HoverContents, HoverParams, LanguageString, MarkedString};
 use rowan::ast::{support::child, AstNode};
-use wat_syntax::ast::GlobalType;
+use wat_syntax::{ast::GlobalType, SyntaxNode};
 
 impl LanguageService {
     pub fn hover(&self, params: HoverParams) -> Option<Hover> {
@@ -33,6 +33,15 @@ impl LanguageService {
         symbol_table
             .find_param_def(&key)
             .or_else(|| symbol_table.find_local_def(&key))
+            .or_else(|| {
+                symbol_table.symbols.iter().find(|symbol| {
+                    symbol.key == key
+                        && matches!(
+                            symbol.kind,
+                            SymbolItemKind::Param(..) | SymbolItemKind::Local(..)
+                        )
+                })
+            })
             .map(|symbol| {
                 let mut content_value = '('.to_string();
                 match &symbol.kind {
@@ -77,35 +86,7 @@ impl LanguageService {
                     Hover {
                         contents: HoverContents::Array(
                             symbols
-                                .map(|symbol| {
-                                    let mut content_value = '('.to_string();
-                                    if let SymbolItemKind::GlobalDef(idx) = &symbol.kind {
-                                        content_value.push_str("global");
-                                        if let Some(name) = &idx.name {
-                                            content_value.push(' ');
-                                            content_value.push_str(name);
-                                        }
-                                    }
-                                    let node = symbol.key.ptr.to_node(&root);
-                                    if let Some(global_type) = child::<GlobalType>(&node) {
-                                        let mutable = global_type.mut_keyword().is_some();
-                                        if mutable {
-                                            content_value.push_str(" (mut");
-                                        }
-                                        if let Some(val_type) = global_type.val_type() {
-                                            content_value.push(' ');
-                                            content_value.push_str(&val_type.syntax().to_string());
-                                        }
-                                        if mutable {
-                                            content_value.push(')');
-                                        }
-                                    }
-                                    content_value.push(')');
-                                    MarkedString::LanguageString(LanguageString {
-                                        language: "wat".into(),
-                                        value: content_value,
-                                    })
-                                })
+                                .map(|symbol| create_global_def_hover(symbol, &root))
                                 .collect(),
                         ),
                         range: Some(helpers::rowan_range_to_lsp_range(
@@ -115,5 +96,53 @@ impl LanguageService {
                     }
                 })
             })
+            .or_else(|| {
+                symbol_table
+                    .symbols
+                    .iter()
+                    .find(|symbol| {
+                        symbol.key == key && matches!(symbol.kind, SymbolItemKind::GlobalDef(..))
+                    })
+                    .map(|symbol| {
+                        let root = self.ctx.root(uri);
+                        Hover {
+                            contents: HoverContents::Scalar(create_global_def_hover(symbol, &root)),
+                            range: Some(helpers::rowan_range_to_lsp_range(
+                                &line_index,
+                                token.text_range(),
+                            )),
+                        }
+                    })
+            })
     }
+}
+
+fn create_global_def_hover(symbol: &SymbolItem, root: &SyntaxNode) -> MarkedString {
+    let mut content_value = '('.to_string();
+    if let SymbolItemKind::GlobalDef(idx) = &symbol.kind {
+        content_value.push_str("global");
+        if let Some(name) = &idx.name {
+            content_value.push(' ');
+            content_value.push_str(name);
+        }
+    }
+    let node = symbol.key.ptr.to_node(root);
+    if let Some(global_type) = child::<GlobalType>(&node) {
+        let mutable = global_type.mut_keyword().is_some();
+        if mutable {
+            content_value.push_str(" (mut");
+        }
+        if let Some(val_type) = global_type.val_type() {
+            content_value.push(' ');
+            content_value.push_str(&val_type.syntax().to_string());
+        }
+        if mutable {
+            content_value.push(')');
+        }
+    }
+    content_value.push(')');
+    MarkedString::LanguageString(LanguageString {
+        language: "wat".into(),
+        value: content_value,
+    })
 }
