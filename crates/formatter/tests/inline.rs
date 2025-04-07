bdab6b9fe0224353aece8ab3807c56fa1101c982
Pig Fang <g-plane@hotmail.com>
2025-04-07 11:42:55 +0800

    test(fmt): add test for single empty line only

---
+++ crates/formatter/tests/inline.rs
@@ -0,0 +1,10 @@
+use rowan::ast::AstNode;
+use wat_formatter::format;
+use wat_syntax::ast::Root;
+
+#[test]
+fn new_line() {
+    let (tree, _) = wat_parser::parse("\n");
+    let output = format(&Root::cast(tree).unwrap(), &Default::default());
+    assert_eq!(output, "\n");
+}
