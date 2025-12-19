37e27c3c4cb7ae9b0e927daefe90573c43c198a1
Pig Fang <g-plane@hotmail.com>
2025-12-19 10:57:43 +0800

    refactor(service): accept multiple exports when collecting exports

--- crates/service/src/exports.rs
+++ crates/service/src/exports.rs
@@ -17,38 +17,39 @@ pub(crate) fn get_exports<'db>(
         .root_tree(db)
         .children()
         .map(|module| {
-            (
-                SyntaxNodePtr::new(&module),
-                module
-                    .children()
-                    .filter_map(|module_field| {
-                        if module_field.kind() == SyntaxKind::MODULE_FIELD_EXPORT {
-                            let name = module_field
-                                .first_child_by_kind(&|kind| kind == SyntaxKind::NAME)?;
-                            helpers::ast::extract_index_from_export(&module_field)
-                                .and_then(|index| {
-                                    symbol_table.resolved.get(&SymbolKey::new(&index))
-                                })
-                                .map(|def_key| Export {
-                                    def_key: *def_key,
-                                    name: name.to_string(),
-                                    range: name.text_range(),
-                                })
-                        } else {
-                            module_field
-                                .first_child_by_kind(&|kind| kind == SyntaxKind::EXPORT)
-                                .and_then(|export| {
-                                    export.first_child_by_kind(&|kind| kind == SyntaxKind::NAME)
-                                })
-                                .map(|name| Export {
-                                    def_key: SymbolKey::new(&module_field),
-                                    name: name.to_string(),
-                                    range: name.text_range(),
-                                })
-                        }
-                    })
-                    .collect(),
-            )
+            let mut exports = Vec::new();
+            module.children().for_each(|module_field| {
+                if module_field.kind() == SyntaxKind::MODULE_FIELD_EXPORT {
+                    if let Some(name) =
+                        module_field.first_child_by_kind(&|kind| kind == SyntaxKind::NAME)
+                        && let Some(def_key) = helpers::ast::extract_index_from_export(
+                            &module_field,
+                        )
+                        .and_then(|index| symbol_table.resolved.get(&SymbolKey::new(&index)))
+                    {
+                        exports.push(Export {
+                            def_key: *def_key,
+                            name: name.to_string(),
+                            range: name.text_range(),
+                        });
+                    }
+                } else {
+                    exports.extend(
+                        module_field
+                            .children()
+                            .filter(|child| child.kind() == SyntaxKind::EXPORT)
+                            .filter_map(|export| {
+                                export.first_child_by_kind(&|kind| kind == SyntaxKind::NAME)
+                            })
+                            .map(|name| Export {
+                                def_key: SymbolKey::new(&module_field),
+                                name: name.to_string(),
+                                range: name.text_range(),
+                            }),
+                    );
+                }
+            });
+            (SyntaxNodePtr::new(&module), exports)
         })
         .collect()
 }
