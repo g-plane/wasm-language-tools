47c346696b97f05321d7fed7c0a5d6978edd1c4e
Pig Fang <g-plane@hotmail.com>
2026-02-02 16:00:20 +0800

    refactor(service): reduce tree traversal when checking const expr

--- crates/service/src/checker/const_expr.rs
+++ crates/service/src/checker/const_expr.rs
@@ -1,29 +1,62 @@
 use super::Diagnostic;
 use crate::data_set::CONST_INSTRS;
-use rowan::{TextRange, ast::AstNode};
-use wat_syntax::{SyntaxNode, ast::Instr};
+use rowan::{
+    TextRange, WalkEvent,
+    ast::{AstNode, support},
+};
+use wat_syntax::{SyntaxKind, SyntaxNode, ast::Instr};
 
 const DIAGNOSTIC_CODE: &str = "const-expr";
 
 pub fn check(node: &SyntaxNode) -> Option<Diagnostic> {
-    let first = node.first_child_by_kind(&Instr::can_cast)?;
-    let last = node.children().filter(|child| Instr::can_cast(child.kind())).last()?;
-    if node.descendants().filter_map(Instr::cast).all(|instr| {
-        if let Instr::Plain(plain) = instr {
-            plain
-                .instr_name()
-                .is_some_and(|instr_name| CONST_INSTRS.contains(&instr_name.text()))
-        } else {
-            false
-        }
-    }) {
-        None
-    } else {
+    let mut first = None;
+    let mut last = None;
+    let mut is_const = true;
+    node.children()
+        .filter(|child| Instr::can_cast(child.kind()))
+        .for_each(|instr| {
+            if first.is_none() {
+                first = Some(instr.clone());
+            }
+            last = Some(instr.clone());
+            let mut preorder = instr.preorder();
+            while let Some(walk_event) = preorder.next() {
+                let WalkEvent::Enter(node) = walk_event else {
+                    continue;
+                };
+                match node.kind() {
+                    SyntaxKind::BLOCK_BLOCK
+                    | SyntaxKind::BLOCK_LOOP
+                    | SyntaxKind::BLOCK_IF
+                    | SyntaxKind::BLOCK_TRY_TABLE => {
+                        is_const = false;
+                        break;
+                    }
+                    SyntaxKind::PLAIN_INSTR => {
+                        if support::token(&node, SyntaxKind::INSTR_NAME)
+                            .is_some_and(|instr_name| !CONST_INSTRS.contains(&instr_name.text()))
+                        {
+                            is_const = false;
+                            break;
+                        }
+                    }
+                    _ => {
+                        preorder.skip_subtree();
+                    }
+                }
+            }
+        });
+    if !is_const
+        && let Some(first) = first
+        && let Some(last) = last
+    {
         Some(Diagnostic {
             range: TextRange::cover(first.text_range(), last.text_range()),
             code: DIAGNOSTIC_CODE.into(),
             message: "expression must be constant".into(),
             ..Default::default()
         })
+    } else {
+        None
     }
 }
